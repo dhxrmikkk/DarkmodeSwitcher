da504a0c55e7309b3d5219bb8e730b98868af543
Dharmik Vadgama <dharmik.ubs@gmail.com>
2024-09-24 13:58:06 +0530

    update

--- src/App.jsx
+++ src/App.jsx
@@ -123,11 +123,11 @@ const App = () => {
               <div className="mt-10 flex items-center justify-center gap-x-6">
                 <a
                   href="#"
-                  className="rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+                  className="rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-normal text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
                 >
                   Get started
                 </a>
-                <a href="#" className="text-sm font-semibold leading-6 text-gray-900 dark:text-white">
+                <a href="#" className="text-sm font-normal leading-6 text-gray-900 dark:text-white">
                   Learn more <span aria-hidden="true">→</span>
                 </a>
               </div>
